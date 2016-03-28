16662bd6a14a44a2584b4b0cf2864549d9be3b50
Leonardo <leomicheloni@hotmail.com>
2016-03-28 14:23:05 -0300

    information updated

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DotNetSlackClient")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simple .net client to publish into Slack channels")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Leonardo Micheloni")]
 [assembly: AssemblyProduct("DotNetSlackClient")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]

a2bf9e2145d6727242ce095a73f713994146daa4
Leonardo <leomicheloni@hotmail.com>
2016-03-28 17:32:09 -0300

    RestSharp dependency removed, all the work done by hand for simplicity

--- src/SlackClient.cs
+++ src/SlackClient.cs
@@ -2,31 +2,47 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DotNetSlackClient
 {
-    class SlackClient
+    /// <summary>
+    /// Sends messages to Slack using this recomendation
+    /// https://api.slack.com/incoming-webhooks
+    /// </summary>
+    public class SlackClient
     {
+        /// <summary>
+        /// The hook URL somehing like https://hooks.slack.com/services/SSSSSSST/KKKKSSSKKKSS/SKuKiKt7S6SjSeKK
+        /// In order to use that you should allow incoming webhooks to your channel
+        /// </summary>
         public string WebHookUrl { get; set; }
-        public string BotName { get; set; }
-        public string BotIcon { get; set; }
-        public string MessageFormat { get; set; }
+
+        /// <summary>
+        /// Sends webhook message to Slack channel (if it is configured correctly)
+        /// For simplicity it uses the default hook channel and name
+        /// </summary>
+        /// <param name="message">The message to send</param>
         public void NotifySlack(string message)
         {
-            var client = new RestClient(WebHookUrl);
-            var request = new RestRequest(Method.POST) { RequestFormat = DataFormat.Json };
-            request.AddBody(
-                new
-                {
-                    text = message,
-                    username = BotName,
-                    icon_emoji = BotIcon,
-                    mrkdwn = true
-                });
+            var request = (HttpWebRequest)WebRequest.Create(this.WebHookUrl);
 
-            client.ExecuteAsync(request, null);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            
+            var bytes = Encoding.UTF8.GetBytes("{\"text\":\"" + message  + "\"}");
+
+            request.ContentLength = bytes.Length;
+
+            using (var writeStream = request.GetRequestStream())
+            {
+                writeStream.Write(bytes, 0, bytes.Length);
+            }
+
+            var response = request.GetResponse();
         }
     }
 }
